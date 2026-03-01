7ce04b6fef4924c5f4db9f4c96277f886e3b8ce1
Reshetov Denis <reshetovdenis@gmail.com>
2026-03-01 10:25:16 +0100

    Adds teacher quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,17 +86,41 @@ export default async function LandingPage({ searchParams }: { searchParams?: Pro
       <StableQuality />
       <QuoteCarousel
         quotes={[
-          { text: "As opposed to many apps which foster interaction and engagement with the technology itself, Slonig fosters interaction and engagement between the learners themselves. As such, this app can help create learner-centered classrooms where learners learn from each other as they take turns adopting the roles of tutor and learner.", byline: "Lee Mackenzie, Senior Lecturer in Education, Liverpool Hope University"},
+          { text: "As opposed to many apps which foster interaction and engagement with the technology itself, Slonig fosters interaction and engagement between the learners themselves. As such, this app can help create learner-centered classrooms where learners learn from each other as they take turns adopting the roles of tutor and learner.", byline: "Lee Mackenzie, Senior Lecturer in Education, Liverpool Hope University" },
           { text: "I have regularly found that when students teach one another and learn from one another, they engage more deeply with the content in my statistics/data science courses. I think a distributed and structured approach such as Slonig offers a solution and fosters a collaborative and educationally beneficial environment in the classroom.", byline: "Amish Mishra, Ph.D., Assistant Professor, Mathematics Department, Taylor University" },
           { text: "Slonig represents a turning point in today's education system and gives a true picture of how important peer learning is, continuously pointing out the benefits of this type of learning, and opens a broader perspective of moving to a higher level, that is, it functions on a large scale and has a tendency to spread.", byline: "Rade Keković, Adviser, Bureau for education Montenegro" },
-          { text: "Aside from learning educational content, learners can develop key social and communication skills. In busy classrooms which are increasingly understaffed and under resourced, using this app can make a real difference to the classroom dynamic, not to mention the confidence and esteem of learners.", byline: "Lee Mackenzie, Senior Lecturer in Education, Liverpool Hope University"},
+          { text: "Aside from learning educational content, learners can develop key social and communication skills. In busy classrooms which are increasingly understaffed and under resourced, using this app can make a real difference to the classroom dynamic, not to mention the confidence and esteem of learners.", byline: "Lee Mackenzie, Senior Lecturer in Education, Liverpool Hope University" },
           { text: "As a computer science professor and education researcher focused on peer instruction and technology-enhanced pedagogy, I appreciate Slonig’s commitment to strengthening human-to-human learning rather than replacing it with AI.", byline: "Steve Geinitz, Associate Professor, Metropolitan State University of Denver" },
         ]}
       />
-      <RequestDemo id="after_efficiency" caption="Request a Demo"/>
+      <RequestDemo id="after_efficiency" caption="Request a Demo" />
       <LessonIntegration />
       <HowSlonigWorks />
       <TeacherWork />
+      <QuoteCarousel
+        quotes={[
+          {
+            text: "Most of the time, students worked on their own. I just observed—and I was surprised they actually learned.",
+            byline: "Kate, high school math teacher",
+          },
+          {
+            text: "The approach used in Slonig helps enhance student motivation, communication, and interaction within the group.",
+            byline: "Nic, high school chemistry teacher",
+          },
+          {
+            text: "Slonig helps children develop skills such as teaching others what they already know, which changes how they perceive new information. I find the use of the Slonig in lessons very beneficial.",
+            byline: "Mary, biology teacher",
+          },
+          {
+            text: "I’ve seen remarkable results using Slonig. Not only did the students genuinely enjoy the experience, but those who took on mentoring roles truly blossomed.",
+            byline: "Elena, ESL teacher",
+          },
+          {
+            text: "Students appreciate the freedom Slonig lessons offer, allowing them to interact with any classmate and move around the classroom. This method is not just beneficial for students; it’s a game-changer for teachers as well.",
+            byline: "Karina, history teacher",
+          },
+        ]}
+      />
 
       <ValueStack />
 
